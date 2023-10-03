422b10a4f099db107f0773e2a0f65a7683ecda7d
unknown <pranathi.borra@gmail.com>
2023-10-03 11:34:04 -0400

    Added function definition for minmax method

--- src/MinMaxCalculation.java
+++ src/MinMaxCalculation.java
@@ -3,12 +3,14 @@ import java.util.*;
 /**
  * A simple class to compute basic, descriptive statistics.
  */
-public class BasicStats {
+public class MinMaxCalculation {
 
   public ArrayList<Double> data = new ArrayList<Double>();
 
-  private int getMinMax() {
+  private double getMinMax(ArrayList<Double> data) {
     
+	  double min = Collections.min(data);
+	  return min;
   }
 
   public static void main() {
