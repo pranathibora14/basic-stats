49c7af385f5425abbc73d10fb3f3bbf88346ff29
eesha-k <erkurode@gmail.com>
2023-10-03 11:26:35 -0400

    Added minmax class

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+/**
+ * A simple class to compute basic, descriptive statistics.
+ */
+public class BasicStats {
+
+  public ArrayList<Double> data = new ArrayList<Double>();
+
+  private int getMinMax() {
+    
+  }
+
+  public static void main() {
+
+  }
+}
